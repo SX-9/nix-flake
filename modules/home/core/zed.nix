d4c0043a057d4fd05aea94c23470e419c12fef35
Satria <admin@satr14.my.id>
2026-02-18 21:20:08 +0700

    tailwind support

--- modules/home/core/zed.nix
+++ modules/home/core/zed.nix
@@ -24,25 +24,36 @@
         light = "Catppuccin Mocha (sapphire)";
         dark = "Catppuccin Mocha (sapphire)";
       };
-      lsp.discord_presence.initialization_options = {
-        application_id = "1263505205522337886";
-        base_icons_url = "https://raw.githubusercontent.com/xhyrom/zed-discord-presence/main/assets/icons/";
-        state = "Working on {filename}";
-        details = "In {workspace}";
-        large_image = "{base_icons_url}/{language:lo}.png";
-        large_text = "{language:u}";
-        small_image = "{base_icons_url}/zed.png";
-        small_text = "Zed";
-        git_integration = true;
-        idle = {
-          timeout = 300;
-          action = "change_activity";
-          state = "Idling";
-          details = "In Zed";
-          large_image = "{base_icons_url}/zed.png";
-          large_text = "Zed";
-          small_image = "{base_icons_url}/idle.png";
-          small_text = "Idle";
+      languages.CSS.language_servers = [
+        "tailwindcss-intellisense-css"
+        "!vscode-css-language-server"
+        "..."
+      ];
+      lsp = {
+        tailwindcss-language-server.settings = {
+          classFunctions = [ "cva" "cx" ];
+          experimental.classRegex = [ "[cls|className]\\s\\:\\=\\s\"([^\"]*)" ];
+        };
+        discord_presence.initialization_options = {
+          application_id = "1263505205522337886";
+          base_icons_url = "https://raw.githubusercontent.com/xhyrom/zed-discord-presence/main/assets/icons/";
+          state = "Working on {filename}";
+          details = "In {workspace}";
+          large_image = "{base_icons_url}/{language:lo}.png";
+          large_text = "{language:u}";
+          small_image = "{base_icons_url}/zed.png";
+          small_text = "Zed";
+          git_integration = true;
+          idle = {
+            timeout = 300;
+            action = "change_activity";
+            state = "Idling";
+            details = "In Zed";
+            large_image = "{base_icons_url}/zed.png";
+            large_text = "Zed";
+            small_image = "{base_icons_url}/idle.png";
+            small_text = "Idle";
+          };
         };
       };
     };
