da908a8e140c2ae8a083760916196c9faa1a4d30
Satria <admin@satr14.my.id>
2026-03-01 22:09:25 +0700

    change bind ip to localhost

--- modules/system/homelab/dash.nix
+++ modules/system/homelab/dash.nix
@@ -72,7 +72,7 @@ in {
     environmentFile = "/var/lib/glance/.env";
     settings = {
       server = {
-        host = "0.0.0.0"; # "127.0.0.1";
+        host = "127.0.0.1";
         port = 5070;
       };
 
