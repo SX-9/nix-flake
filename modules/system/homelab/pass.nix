54a5363c08e6c925c94a0294e58d2a815061abef
Satria <admin@satr14.my.id>
2026-03-06 16:16:09 +0700

    change vaultwarden backup dir to mnt

--- modules/system/homelab/pass.nix
+++ modules/system/homelab/pass.nix
@@ -1,12 +1,15 @@
-{ homelab, ... }: {
+{ homelab, ... }: let
+  app-url = "https://pass.proxy.${homelab.domain}";
+in {
   services.vaultwarden = {
     enable = true;
+    domain = app-url;
+    backupDir = "/mnt/data/vaultwarden";
     config = {
-      DOMAIN = "https://pass.proxy.${homelab.domain}";
+      DOMAIN = app-url;
       ROCKET_PORT = 8060;
       ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_LOG = "critical";
-      DATA_FOLDER = "/mnt/data/vaultwarden";
       SIGNUPS_ALLOWED = true;
     };
   };
